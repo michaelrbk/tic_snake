e480f8e0947f0566cb2bab64015d68a1c300484d
michaelrbecker <michael.becker@bairesdev.com>
2025-07-15 07:32:17 -0300

    fix move back from direction it was coming from

--- tic_snake.lua
+++ tic_snake.lua
@@ -2,33 +2,34 @@
 -- author:  Michael Becker michaelrbk@gmail.com
 -- desc:    first snake version on TIC-80
 -- license: MIT License (change this to your license of choice)
--- version: 0.1
+-- version: 0.3
 -- script:  lua
 
 -- GLOBALS
-CELL_SIZE=16
-GRID_SIZE=8
-TOP_BORDER=4
-LEFT_BORDER=54
+CELL_SIZE = 16
+GRID_SIZE = 8
+TOP_BORDER = 4
+LEFT_BORDER = 54
 X_DIR = 1
 Y_DIR = 0
-T= CELL_SIZE
-DIR_NEW = 1
+T = CELL_SIZE
+DIR_NEW = -1
+DIR = 1
 SCORE = 0
-END = false
+END = true
 NEW_FRUIT = false
 
-snake_head ={
-	x_pos=2,
-	y_pos=2
+snake_head = {
+	x_pos = 3,
+	y_pos = 3
 }
 
-snake_tail ={
+snake_tail = {
 }
 
 fruit = {
-	x_pos=0,
-	y_pos=0
+	x_pos = 0,
+	y_pos = 0
 }
 
 function INIT()
@@ -36,164 +37,158 @@ function INIT()
 end
 
 function TIC()
- INPUTS()
- UPDATES()
- DRAW()
+	INPUTS()
+	UPDATES()
+	DRAW()
 end
 
 function INPUTS()
-	if btn(0) then DIR_NEW = 0 end -- up
-	if btn(3) then DIR_NEW = 1 end -- right
-	if btn(1) then DIR_NEW = 2 end -- down
-	if btn(2) then DIR_NEW = 3 end -- left
+	if btn(0) and DIR ~= 2 then DIR_NEW = 0 end -- up
+	if btn(3) and DIR ~= 3 then DIR_NEW = 1 end -- right
+	if btn(1) and DIR ~= 0 then DIR_NEW = 2 end -- down
+	if btn(2) and DIR ~= 1 then DIR_NEW = 3 end -- left
 end
 
 function UPDATES()
- 	T=T-1
- 	if T== 0 then
+	T = T - 1
+	if T == 0 then
 		move()
 		T = CELL_SIZE
-	end 
+	end
 	check_fruit()
 	check_endgame()
 end -- UPDATES
 
 function DRAW()
 	cls(0)
-  
-	if(END) then
-   		new_game()
+	draw_background()
+	if (END) then
+		if DIR_NEW == -1 then
+			return
+		end
+		new_game()
 	end
-  
+
 	-- grids
-  	for _x=LEFT_BORDER,LEFT_BORDER+CELL_SIZE*GRID_SIZE-1, CELL_SIZE do
-		for _y=TOP_BORDER,TOP_BORDER+CELL_SIZE*GRID_SIZE-1, CELL_SIZE do
-			rectb(_x ,_y, CELL_SIZE, CELL_SIZE, 12)
+	for _x = LEFT_BORDER, LEFT_BORDER + CELL_SIZE * GRID_SIZE - 1, CELL_SIZE do
+		for _y = TOP_BORDER, TOP_BORDER + CELL_SIZE * GRID_SIZE - 1, CELL_SIZE do
+			--rectb(_x ,_y, CELL_SIZE, CELL_SIZE, 12)
+			spr(256, _x, _y, -1, 1, 0, 0, 1, 1)
+			spr(256, _x + 7, _y + 7, -1, 1, 0, 0, 1, 1)
+			spr(256, _x, _y + 7, -1, 1, 0, 0, 1, 1)
+			spr(256, _x + 7, _y, -1, 1, 0, 0, 1, 1)
 		end
 	end
-		
+
 	-- snake
-	rect(xgrid(snake_head.x_pos),ygrid(snake_head.y_pos),CELL_SIZE,CELL_SIZE,12)
+	rect(xgrid(snake_head.x_pos), ygrid(snake_head.y_pos), CELL_SIZE, CELL_SIZE, 12)
 
 	-- fruit
-	rect(xgrid(fruit.x_pos),ygrid(fruit.y_pos),CELL_SIZE,CELL_SIZE,11)
+	rect(xgrid(fruit.x_pos), ygrid(fruit.y_pos), CELL_SIZE, CELL_SIZE, 11)
 
 	-- snake tail
-	for i=1, #snake_tail do
+	for i = 1, #snake_tail do
 		rect(xgrid(snake_tail[i].x_pos), ygrid(snake_tail[i].y_pos), CELL_SIZE, CELL_SIZE, 13)
 	end
+end -- DRAW
 
-	-- background 
-		
+function draw_background()
 	-- spr(id x y colorkey=-1 scale=1 flip=0 rotate=0 w=1 h=1)
 	-- pig sprintes are 64 and 66, change every half a second
 	sprite = ((math.floor(time() / 500) % 2) * 2)
-	spr(320+sprite,22,14 ,-1,  2,0 ,0,2 ,2)
-		
+	spr(320 + sprite, 22, 14, -1, 2, 0, 0, 2, 2)
+
 	-- cat
-	spr(356+sprite,185,14 ,-1, 2,1 ,0,2 ,2)
-
-	print('x'..snake_head.x_pos,3,0)
-	print('y'..snake_head.y_pos,3,15)
-
-	print('SCORE '..SCORE,3,47,12)
-	for i=1, #snake_tail do
-		local snake_body_part = snake_tail[i]
-		print('tail x'..snake_body_part.x_pos,3,30+i*5,13)
-		print('tail y'..snake_body_part.y_pos,3,45+i*5,13)
-	end
-end -- DRAW
+	spr(356 + sprite, 185, 14, -1, 2, 1, 0, 2, 2)
+	print('SCORE ' .. SCORE, 3, 47, 12)
+end
 
 function new_game()
 	SCORE = 0
 	X_DIR = 1
 	Y_DIR = 0
-	T= CELL_SIZE
-	DIR_NEW = 1
+	T = CELL_SIZE
+	DIR = 1
+	DIR_NEW = -1
 	END = false
 	NEW_FRUIT = false
-	snake_head.x_pos = 2
-	snake_head.y_pos = 2
+	snake_head.x_pos = 3
+	snake_head.y_pos = 3
 	spawn_fruit()
-	snake_tail ={
- }
- math.randomseed(time())
+	snake_tail = {
+	}
+	math.randomseed(time())
 end
 
 function xgrid(val)
-	return LEFT_BORDER + (val*CELL_SIZE)
+	return LEFT_BORDER + (val * CELL_SIZE)
 end
 
 function ygrid(val)
-	return TOP_BORDER + (val*CELL_SIZE)
+	return TOP_BORDER + (val * CELL_SIZE)
 end
 
 function move()
 	local x_dir = 0
 	local y_dir = 0
-	if DIR_NEW == 0 then --up
+	DIR = DIR_NEW
+	if DIR_NEW == 0 then  --up
 		y_dir = -1
 	elseif DIR_NEW == 1 then --right
 		x_dir = 1
 	elseif DIR_NEW == 2 then --down
 		y_dir = 1
 	elseif DIR_NEW == 3 then --left
-	 x_dir = -1
+		x_dir = -1
 	end
-	
-		-- move tail
+
+	-- move tail
 	move_tail()
-	
+
 	snake_head.x_pos = snake_head.x_pos + x_dir
 	snake_head.y_pos = snake_head.y_pos + y_dir
-	
-	-- check for tail 
-	for i=1, #snake_tail do 
-		if(snake_head.x_pos == snake_tail[i].x_pos and snake_head.y_pos == snake_tail[i].y_pos) then
+
+	-- check for tail
+	for i = 1, #snake_tail do
+		if (snake_head.x_pos == snake_tail[i].x_pos and snake_head.y_pos == snake_tail[i].y_pos) then
 			END = true
 		end
 	end
-
 end --move
 
 function move_tail()
-
 	snake_part = {
-			x_pos = snake_head.x_pos,
-			y_pos = snake_head.y_pos
-		}
+		x_pos = snake_head.x_pos,
+		y_pos = snake_head.y_pos
+	}
 	if NEW_FRUIT then
 		NEW_FRUIT = false
 		table.insert(snake_tail, 1, snake_part)
-		else
-			if #snake_tail == 0 then
-				return
-			end
- 		table.insert(snake_tail, 1, snake_part)
-			table.remove(snake_tail)
+	else
+		if #snake_tail == 0 then
+			return
+		end
+		table.insert(snake_tail, 1, snake_part)
+		table.remove(snake_tail)
 	end
-	
-
-
-
 end --move_tail
 
 function spawn_fruit()
 	-- Generate random position for fruit
-	fruit.x_pos = math.random(0, GRID_SIZE-1)
-	fruit.y_pos = math.random(0, GRID_SIZE-1)
-	
+	fruit.x_pos = math.random(0, GRID_SIZE - 1)
+	fruit.y_pos = math.random(0, GRID_SIZE - 1)
+
 	-- Prevent fruit from spawning on snake
 	while fruit.x_pos == snake_head.x_pos and fruit.y_pos == snake_head.y_pos do
-		fruit.x_pos = math.random(0, GRID_SIZE-1)
-		fruit.y_pos = math.random(0, GRID_SIZE-1)
+		fruit.x_pos = math.random(0, GRID_SIZE - 1)
+		fruit.y_pos = math.random(0, GRID_SIZE - 1)
 	end
 end
 
 function check_fruit()
 	if snake_head.x_pos == fruit.x_pos and snake_head.y_pos == fruit.y_pos then
 		-- Snake ate the fruit, spawn a new one
-		SCORE =SCORE+1
+		SCORE = SCORE + 1
 		-- Add a new body part to the snake_tail
 		local snake_part = {
 			x_pos = snake_head.x_pos,
@@ -206,17 +201,20 @@ function check_fruit()
 end
 
 function check_endgame()
- 
-	if(snake_head.x_pos > GRID_SIZE-1 or snake_head.x_pos < 0) then
+	if (snake_head.x_pos > GRID_SIZE - 1 or snake_head.x_pos < 0) then
 		END = true
 	end
-	if(snake_head.y_pos > GRID_SIZE-1 or snake_head.y_pos < 0) then
+	if (snake_head.y_pos > GRID_SIZE - 1 or snake_head.y_pos < 0) then
 		END = true
 	end
-	
+	if END then
+		cls(0)
+		NEW_DIR = -1
+	end
 end --check_endgame
 
 -- <SPRITES>
+-- 000:0006000000000000000000700000000000000000600000000070000500000000
 -- 064:0000000c0000000cc00000000c00000000c0000c0c0ccc0c0cccc0ccccccc0c0
 -- 065:c0000cc00cccc0c0c0cc0c00c0cc0c00ccccccc0c0000ccc0cccc0ccc0cc0c0c
 -- 066:0000000c0000000c000000000c00000000c0000c0c0ccc0c0cccc0ccccccc0c0
@@ -276,4 +274,3 @@ end --check_endgame
 -- <PALETTE>
 -- 000:1a1c2c5d275db14053ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
 -- </PALETTE>
-
